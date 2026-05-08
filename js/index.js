c845e0df8fdcdbdeba03b3cd91ac8ad3b5209ae9
gdavidd19 <gabrieldavidcondenunez@gmail.com>
2026-05-08 19:19:39 -0400

    listado de perfiles dinámico usando javascript

--- js/index.js
+++ js/index.js
@@ -4,3 +4,20 @@ document.getElementById('campoBusqueda').placeholder = `${config.name}...`;
 document.getElementById('botonBusqueda').innerHTML = `${config.search}`;
 document.getElementsByTagName('footer')[0].innerHTML = `${config.copyRight}`;
 document.getElementById('semestre').innerHTML = `${config.semester}`;
+
+var cantPerfiles = profiles.length;
+for (let i = 0; i < cantPerfiles; i++) {
+    document.getElementById("perfiles").innerHTML +=
+        `<div class="cajaPerfil">
+            <img src="${profiles[i].ci}/${profiles[i].ci}Big${profiles[i].image_ext}" id="fotoPerfilBig" class="fotoPerfil">
+            <img src="${profiles[i].ci}/${profiles[i].ci}Small${profiles[i].image_ext}" id="fotoPerfilSmall" class="fotoPerfil">
+            <div class="cajaNombrePerfil"><p class="nombrePerfil">${profiles[i].name}</p></div>
+            <hr class="lineaInferior">
+        </div>`;
+}
+perfiles = document.getElementsByClassName('cajaPerfil');
+for (let i = 0; i < cantPerfiles; i++) {
+    perfiles[i].addEventListener("click", function () {
+        window.location.href = `profile.html?ci=${profiles[i].ci}`;
+    });
+}
